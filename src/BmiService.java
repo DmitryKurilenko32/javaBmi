72501bdda7699b7b0749bd188e8d2847fd7359e6
Dmitry <kurilenko907@gmail.com>
2024-02-27 23:38:06 +0300

    rename method

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int weight(double kg, double sm) {
+    public int bmi(double kg, double sm) {
         double index;
         index = kg / (sm * sm);
         return (int) index;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService calculate = new BmiService();
-        int bmi = calculate.weight(98, 1.87);
-        System.out.println("индекс массы тела = " + bmi);
+        int index = calculate.bmi(98, 1.87);
+        System.out.println("индекс массы тела = " + index);
     }
 }
